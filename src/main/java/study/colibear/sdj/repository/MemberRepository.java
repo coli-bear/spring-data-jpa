f34df45fb9eced7603ecc95a15292e89f163406a
coli-bear <icraft@baggeontaeui-MacBookPro.local>
2022-07-28 23:11:00 +0900

    2022-07-28 study

--- src/main/java/study/colibear/sdj/repository/MemberRepository.java
+++ src/main/java/study/colibear/sdj/repository/MemberRepository.java
@@ -3,5 +3,7 @@ package study.colibear.sdj.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.colibear.sdj.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
--- src/test/java/study/colibear/sdj/repository/MemberRepositoryTest.java
+++ src/test/java/study/colibear/sdj/repository/MemberRepositoryTest.java
@@ -8,6 +8,8 @@ import study.colibear.sdj.entity.Member;
 
 import javax.transaction.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -32,5 +34,33 @@ class MemberRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = Member.builder().username("member1").age(10).build();
+        Member member2 = Member.builder().username("member2").age(20).build();
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        List<Member> all = memberRepository.findAll();
+
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long count2 = memberRepository.count();
+        assertThat(count2).isEqualTo(0);
     }
 }
